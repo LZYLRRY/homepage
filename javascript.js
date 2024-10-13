5025006fbed8a750ece55b25584c92b6b5101eb3
LZYLRRY <162039530+LZYLRRY@users.noreply.github.com>
2024-10-13 02:22:38 +0200

    Delete create folder

--- javascript.js
+++ javascript.js
@@ -58,41 +58,14 @@ async function type() {
     span3.classList.add(classCommand);
     div3.appendChild(span3);
 
-    await wait(1000);
-    await typePrompt('mkdir About Contact');
-
-    createFolders();
+    wait(1000).then(() => {
+        typePrompt('mkdir About Contact');
+    });
 }
 
 function wait(tijdInMs) {
     return new Promise((resolve) => {
         setTimeout(resolve, tijdInMs);
-    });
-}
-
-function createFolders() {
-    const folderNames = ['About', 'Contact'];
-
-    folderNames.forEach(folderName => {
-        // Create a new div for the folder
-        const folderDiv = document.createElement('div');
-        folderDiv.classList.add('folder');
-
-        // Create SVG for the folder icon
-        folderDiv.innerHTML = `
-            <svg width="50" height="50" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M10 4H2V20H22V8H12L10 4Z" fill="#FFD700" stroke="black" stroke-width="2"/>
-            </svg>
-            <span>${folderName}</span>
-        `;
-
-        // Append the folder div to the container
-        container.appendChild(folderDiv);
-
-        // Add a click event listener if needed
-        folderDiv.addEventListener('click', () => {
-            console.log(`${folderName} folder clicked!`);
-        });
     });
 }
 
